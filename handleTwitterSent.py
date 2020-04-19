7266633d7ec1efc7b3a897ed58927521b720ab27
Joshan Bajaj <jbajaj1@jhu.edu>
2020-04-18 22:06:50 -0400

    added handling for non existing word, made train, predict, evaluate, and now am testing model on all the given data

--- handleTwitterSent.py
+++ handleTwitterSent.py
@@ -1,11 +1,14 @@
 import torch
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import pandas as pd
 import torch.nn as nn
 import torch.utils.data as data
 
+from torch.utils.data import TensorDataset, DataLoader
 
-def load_tweets(filename):
+
+def load_tweets(filename, initVoc=False):
     X = []
     y = []
     r = open(filename, 'r')
@@ -13,20 +16,33 @@ def load_tweets(filename):
         line = line.split()
         y.append(line[0])
         X.append(line[1:])
-    return X, y
+
+    tokenizedTweets = []
+    tokenizedLabels = []
+    labelDic = {"negative":0, "neutral":1, "positive":2}
+    if initVoc:
+        for t in X:
+            twitterVoc.add_sentence(t)
+    for l in y:
+        tokenizedLabels.append(labelDic[l])
+    for t in X:
+        tokenizedTweets.append(twitterVoc.sentence_to_vec(t))
+
+    tokenizedTweets = torch.LongTensor(tokenizedTweets)
+    tokenizedLabels = torch.LongTensor(tokenizedLabels)
+
+    return tokenizedTweets, tokenizedLabels
+
 
 class Vocab:
-    #variables weren't working for some reason
-    #p_t = 0   # Used for padding short sentences
-    #s_t = 1   # Start-of-sentence token
-    #e_t = 2   # End-of-sentence token
+
 
     def __init__(self, name):
         self.name = name
         self.word2index = {}
         self.word2count = {}
-        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
-        self.num_words = 3
+        self.index2word = {0: "PAD", 1: "UNK"}
+        self.num_words = 2
         self.num_sentences = 0
         self.longest_sentence = 0
 
@@ -60,6 +76,8 @@ class Vocab:
         return self.index2word[index]
 
     def to_index(self, word):
+        if word not in self.word2index:
+            return self.word2index["UNK"]
         return self.word2index[word]
 
     def sentence_to_vec(self, sentence):
@@ -102,24 +120,78 @@ class LSTM(torch.nn.Module):
 
         return output_dist
 
+def validate(expected, predictions):
+    '''
+    totdiff = 0
+    numdiff = 0
+    counter = 0
+    for i in expected:
+        diff = abs(i-predictions[counter])
+        totdiff += diff
+        if diff != 0:
+            numdiff += 1
+        counter += 1
+    return totdiff, numdiff
+    '''
+    return confusion_matrix(expected, predictions)
+
+
+
+
 
 
 twitterVoc = Vocab("twitter")
 
-tweets = load_tweets("../twitter_sentiment/semeval_train.txt")
-tokenizedTweets = []
-for t in tweets[0]:
-    twitterVoc.add_sentence(t)
-for t in tweets[0]:
-    tokenizedTweets.append(twitterVoc.sentence_to_vec(t))
-tokenizedTweets = torch.LongTensor(tokenizedTweets)
+tokenizedTweets, tokenizedLabels = load_tweets("../twitter_sentiment/semeval_train.txt",  initVoc=True)
 
 print(twitterVoc.to_word(4))
 print(twitterVoc.to_index("this"))
 
 print(twitterVoc.num_words)
 
+ourLSTM = LSTM(twitterVoc.num_words, 64, 64)
 
-ourLSTM = LSTM(twitterVoc.num_words, 64, 4)
+#ourLSTM.forward(tokenizedTweets)
 
-ourLSTM.forward(tokenizedTweets[:2])
+
+##################
+#####Training#####
+##################
+
+opt = torch.optim.Adam(ourLSTM.parameters(), lr=.1)
+loss = torch.nn.CrossEntropyLoss()
+epochs = 100
+dataset = DataLoader(TensorDataset(tokenizedTweets, tokenizedLabels), batch_size=100)
+for i in range(epochs):
+    print("Training on epoch", i)
+    for batchidx, (x, y) in enumerate(dataset):
+        opt.zero_grad()
+        outputs = ourLSTM(x)
+        lossVal = loss(outputs, y)
+        lossVal.backward()
+        opt.step()
+
+
+##################
+#####Predict######
+##################
+
+
+predVal = ourLSTM(tokenizedTweets).argmax(dim=-1)
+
+print("Results for Train Data:", validate(tokenizedLabels, predVal))
+
+
+filelist = ["../twitter_sentiment/Twitter2013_raw.txt", "../twitter_sentiment/Twitter2013_raw", "../twitter_sentiment/Twitter2015_raw", "../twitter_sentiment/Twitter2016_raw"]
+
+for file in filelist:
+    tokTestTweets, tokTestLabels = load_tweets(file)
+    predVal = ourLSTM(tokTestTweets).argmax(dim=-1)
+    print("Results for", file, validate(tokTestLabels, predVal))
+
+
+
+
+
+
+
